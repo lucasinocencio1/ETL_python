517994240208a464c57c35335849fa9dc37dc886
Lucas Inocencio <lucasinovo13@gmail.com>
2025-10-19 12:04:51 +0100

    Adicionar funcao que cria pasta output para salvar resultados

--- ETL/src/etl.py
+++ ETL/src/etl.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import glob
+from pathlib import Path
 
 
 # uma funcao de extract que le e consolida os json
@@ -29,15 +30,31 @@ def calcular_kpi_de_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
     df["juros"] = df["Quantidade"] * df["Venda"] * 0.05 #juros de 5%
     return df
 
+# def carregar_dados(df: pd.DataFrame, format_saida: list):
+#     """
+#     parametro que vai ser ou "csv" ou "parquet" ou "os dois"
+#     """
+#     for formato in format_saida:
+#         if formato == 'csv':
+#             df.to_csv("dados.csv", index=False)
+#         if formato == 'parquet':
+#             df.to_parquet("dados.parquet", index=False)
+
+
 def carregar_dados(df: pd.DataFrame, format_saida: list):
     """
-    parametro que vai ser ou "csv" ou "parquet" ou "os dois"
+    Função que carrega os dados em csv ou parquet e cria pastas output/csv e output/parquet
     """
+    output_dir =  Path("output")
+    (output_dir / "csv").mkdir(parents=True, exist_ok=True)
+    (output_dir / "parquet").mkdir(parents=True, exist_ok=True)
     for formato in format_saida:
         if formato == 'csv':
-            df.to_csv("dados.csv", index=False)
-        if formato == 'parquet':
-            df.to_parquet("dados.parquet", index=False)
+            df.to_csv(output_dir / "csv" / "dados.csv", index=False)
+            print(f"Dados carregados em csv com sucesso")
+        elif formato == 'parquet':
+            df.to_parquet(output_dir / "parquet" / "dados.parquet", index=False)
+            print(f"Dados carregados em parquet com sucesso")
 
 
 def pipeline_calcular_kpi_de_vendas_consolidado(pasta: str, formato_de_saida: list):
