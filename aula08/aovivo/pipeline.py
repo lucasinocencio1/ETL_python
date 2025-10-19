02d06e963ba94df26e5ef9c9ca60ee7fffdb80aa
Lucas Inocencio <lucasinovo13@gmail.com>
2025-10-19 12:07:44 +0100

    remove pasta aula08 do projeto

--- aula08/aovivo/pipeline.py
+++
@@ -1,6 +0,0 @@
-from etl import pipeline_calcular_kpi_de_vendas_consolidado
-
-pasta: str = 'data'
-formato_de_saida: list = ["csv", "parquet"]
-
-pipeline_calcular_kpi_de_vendas_consolidado(pasta, formato_de_saida)
