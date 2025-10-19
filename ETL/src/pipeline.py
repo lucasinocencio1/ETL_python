eed0659c5eb379ddd17a05965f5b77ef63b0d1b8
Lucas Inocencio <lucasinovo13@gmail.com>
2025-10-19 14:15:58 +0100

    Adicionando logs

--- ETL/src/pipeline.py
+++ ETL/src/pipeline.py
@@ -1,6 +1,17 @@
 from etl import pipeline_calcular_kpi_de_vendas_consolidado
+#import logging
+from loguru import logger
 
+
+#logs usando loguru ao inves de logging
+logger.add("logs/pipeline.log")
+logger.info("Iniciando pipeline de ETL")
+logger.debug("Pipeline de ETL iniciada")
+logger.warning("Pipeline de ETL iniciada")
+logger.error("Pipeline de ETL iniciada")
+logger.critical("Pipeline de ETL iniciada")
+
+#pipeline de ETL
 pasta: str = 'data'
 formato_de_saida: list = ["csv", "parquet"]
-
 pipeline_calcular_kpi_de_vendas_consolidado(pasta, formato_de_saida)
